2a77e29df1177a1480f13f9aaa6a0884cf1f7c1d
Germán Lena <german.lena@gmail.com>
2014-09-25 21:37:39 -0300

    A0 widget

--- templates/login-form.php
+++ templates/login-form.php
@@ -4,11 +4,13 @@ function renderAuth0Form($canShowLegacyLogin = true)
     if(is_user_logged_in())
         return;
 
-$wordpress_login_enabled = WP_Auth0_Options::get('wordpress_login_enabled') == 1;
-if (!$wordpress_login_enabled || !isset($_GET['wle'])) {
-    include ('auth0-login-form.php');
-}else{
-    add_action('login_footer', array('WP_Auth0', 'render_back_to_auth0'));
+    $wordpress_login_enabled = $canShowLegacyLogin && (WP_Auth0_Options::get('wordpress_login_enabled') == 1);
+
+    if (!$wordpress_login_enabled || !isset($_GET['wle'])) {
+        include ('auth0-login-form.php');
+    }else{
+        add_action('login_footer', array(WP_Auth0::class, 'render_back_to_auth0'));
+    }
 }
 
 ?>
