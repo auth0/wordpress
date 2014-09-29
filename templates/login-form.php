b823c8d49ee811eaa2b5b315551b0483720c3370
Germán Lena <german.lena@gmail.com>
2014-09-29 19:35:22 -0300

    Fix for legacy php versions

--- templates/login-form.php
+++ templates/login-form.php
@@ -9,7 +9,7 @@ function renderAuth0Form($canShowLegacyLogin = true)
     if (!$wordpress_login_enabled || !isset($_GET['wle'])) {
         require_once ('auth0-login-form.php');
     }else{
-        add_action('login_footer', array(WP_Auth0::class, 'render_back_to_auth0'));
+        add_action('login_footer', array('WP_Auth0', 'render_back_to_auth0'));
     }
 }
 
