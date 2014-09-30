2e57d26b7cb92c5be2d52db23ff547df72d8020c
Germán Lena <german.lena@gmail.com>
2014-09-29 22:43:44 -0300

    agregado de configuracion fix grafico

--- templates/auth0-login-form.php
+++ templates/auth0-login-form.php
@@ -58,24 +58,35 @@ if(empty($client_id) || empty($domain)): ?>
         callback = a0_wp_login.initialize
     }
 
-    var widget = new Auth0Widget({
-        domain:     '<?php echo $domain; ?>',
-        chrome: true,
-        clientID:       '<?php echo $client_id; ?>',
+    var lock = new Auth0Lock('<?php echo $client_id; ?>', '<?php echo $domain; ?>');
+    var options = {
         callbackURL:    '<?php echo site_url('/index.php?auth0=1'); ?>',
         container:      'auth0-login-form',
-        state:          '<?php echo $state; ?>',
-        showSignup:     <?php echo $allow_signup?'true':'false' ?>,
-        dict:           { signin: { title: '<?php echo $title ?>' } }
-    });
+        authParams: {
+            state:      '<?php echo $state; ?>'
+        },
+        dict: {
+            signin: {
+                title: '<?php echo $title ?>'
+            }
+        },
+        socialBigButtons: <?php echo ($social_big_buttons ? 'true' : 'false') ;?>,
+        gravatar: <?php echo ($gravatar ? 'true' : 'false') ;?>,
+        usernameStyle: '<?php echo $username_style;?>',
+    };
 
-    widget.signin({
-        onestep: true,
-        theme: 'static',
-        standalone: true,
-        showIcon: <?php echo ($show_icon ? 'true' : 'false'); ?>,
-        icon: '<?php echo ($show_icon ? WP_Auth0_Options::get('icon_url') : ''); ?>'
-    }, callback);
+    <?php if ($show_icon) { ?>
+        options['icon'] = WP_Auth0_Options::get('icon_url');
+    <?php } ?>
+
+    <?php if ($allow_signup) { ?>
+        lock.show(options, callback);
+    <?php } else { ?>
+        lock.showSignin(options, callback);
+    <?php } ?>
+
+
+
 
 </script>
 <?php
