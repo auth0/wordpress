9973cbcedf5e0bff720afedf09df217e46747a66
Germán Lena <german.lena@gmail.com>
2014-09-30 21:44:43 -0300

    added dict + extra settings

--- templates/auth0-login-form.php
+++ templates/auth0-login-form.php
@@ -5,12 +5,14 @@ $show_icon = absint(WP_Auth0_Options::get('show_icon'));
 $cdn = WP_Auth0_Options::get('cdn_url');
 $allow_signup = WP_Auth0_Options::get('allow_signup') == 1;
 $extra_css = apply_filters( 'auth0_login_css', '');
-
 $dict = WP_Auth0_Options::get('dict');
 $username_style = WP_Auth0_Options::get('username_style');
 $social_big_buttons = WP_Auth0_Options::get('social_big_buttons') == 1;
 $gravatar = WP_Auth0_Options::get('gravatar') == 1;
 $remember_last_login = WP_Auth0_Options::get('remember_last_login') == 1;
+$title = WP_Auth0_Options::get('form_title');
+$extra_conf = WP_Auth0_Options::get('extra_conf');
+
 
 if (isset($_GET['interim-login']) && $_GET['interim-login'] == 1) {
     $interim_login = true;
@@ -19,7 +21,6 @@ if (isset($_GET['interim-login']) && $_GET['interim-login'] == 1) {
 }
 
 // Get title for login widget
-$title = WP_Auth0_Options::get('form_title');
 if (empty($title)) {
     $title = "Sign In";
 }
@@ -58,22 +59,33 @@ if(empty($client_id) || empty($domain)): ?>
 
 <?php
 
-    $optionsObj = array(
-        "callbackURL"   =>  site_url('/index.php?auth0=1'),
-        "container"     =>  'auth0-login-form',
-        "authParams"    => array("state" => $state),
-        "dict" => array(
+    if (trim($dict) == '')
+    {
+        $dict = array(
             "signin" => array(
                 "title" => $title
             )
-        ),
+        );
+    }
+
+    $options_obj = array(
+        "callbackURL"   =>  site_url('/index.php?auth0=1'),
+        "container"     =>  'auth0-login-form',
+        "authParams"    => array("state" => $state),
+        "dict"          => $dict,
         "socialBigButtons"  => $social_big_buttons,
         "gravatar"          => $gravatar,
         "usernameStyle"     => $username_style,
         "rememberLastLogin" => $remember_last_login
     );
 
-    $options = json_encode($optionsObj);
+    if (trim($extra_conf) != '')
+    {
+        $extra_conf_arr = json_decode($extra_conf, true);
+        $options_obj = array_merge( $extra_conf_arr, $options_obj  );
+    }
+
+    $options = json_encode($options_obj );
 ?>
     var options = <?php echo $options; ?>;
 
@@ -82,9 +94,6 @@ if(empty($client_id) || empty($domain)): ?>
     <?php } else { ?>
         lock.showSignin(options, callback);
     <?php } ?>
-
-
-
 
 </script>
 <?php
