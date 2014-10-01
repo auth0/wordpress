106a829d6f61d4028306fdcdde576c70c7fda305
Germán Lena <german.lena@gmail.com>
2014-10-01 19:38:47 -0300

    fix hidden WP login form when the plugin is disabled

--- templates/auth0-login-form.php
+++ templates/auth0-login-form.php
@@ -1,20 +1,13 @@
 <?php
-$activated = absint(WP_Auth0_Options::get( 'active' )) == 1;
 $client_id = WP_Auth0_Options::get('client_id');
 $domain = WP_Auth0_Options::get('domain');
+$show_icon = absint(WP_Auth0_Options::get('show_icon'));
 $cdn = WP_Auth0_Options::get('cdn_url');
 $allow_signup = WP_Auth0_Options::get('allow_signup') == 1;
 $extra_css = apply_filters( 'auth0_login_css', '');
-$showAsModal = (isset($specialSettings['show_as_modal']) && $specialSettings['show_as_modal'] == 1);
-$modalTriggerName = 'Login';
-if (isset($specialSettings['modal_trigger_name']) && $specialSettings['modal_trigger_name'] != '')
-{
-    $modalTriggerName = $specialSettings['modal_trigger_name'];
-}
+$activated = absint(WP_Auth0_Options::get( 'active' )) == 1;
 
-if (isset($specialSettings['show_as_modal'])) unset($specialSettings['show_as_modal']);
-if (isset($specialSettings['modal_trigger_namemodal_trigger_name'])) unset($specialSettings['modal_trigger_name']);
-
+$form_desc = WP_Auth0_Options::get('form_desc');
 if (isset($_GET['interim-login']) && $_GET['interim-login'] == 1) {
     $interim_login = true;
 } else {
@@ -22,6 +15,7 @@ if (isset($_GET['interim-login']) && $_GET['interim-login'] == 1) {
 }
 
 // Get title for login widget
+$title = WP_Auth0_Options::get('form_title');
 if (empty($title)) {
     $title = "Sign In";
 }
@@ -29,80 +23,52 @@ if (empty($title)) {
 $stateObj = array("interim" => $interim_login, "uuid" =>uniqid());
 $state = json_encode($stateObj);
 
-if(empty($client_id) || empty($domain)){ ?>
-
-    <p><?php _e('Auth0 Integration has not yet been set up! Please visit your Wordpress Auth0 settings and fill in the required settings.', WPA0_LANG); ?></p>
-
-<?php } else { ?>
-    <div id="form-signin-wrapper" class="auth0-login">
-        <?php include 'error-msg.php'; ?>
-        <div class="form-signin">
-
-            <?php if ($showAsModal) { ?>
-
-                <button id="a0LoginButton" onclick="a0ShowLoginModal();" ><?php echo $modalTriggerName; ?></button>
-
-            <?php } else { ?>
-                <div id="auth0-login-form">
-                </div>
-            <?php } ?>
-            <?php if ($activated && $wordpress_login_enabled && $canShowLegacyLogin) { ?>
-                <div id="extra-options">
-                    <a href="?wle">Login with WordPress username</a>
-                </div>
-            <?php } ?>
+if(empty($client_id) || empty($domain)): ?>
+<p><?php _e('Auth0 Integration has not yet been set up! Please visit your Wordpress Auth0 settings and fill in the required settings.', WPA0_LANG); ?></p>
+<?php else: ?>
+<div id="form-signin-wrapper" class="auth0-login">
+    <?php include 'error-msg.php'; ?>
+    <div class="form-signin">
+        <div id="auth0-login-form">
         </div>
+        <?php if ($wordpress_login_enabled && $activated): ?>
+            <div id="extra-options">
+                <a href="?wle">Login with WordPress username</a>
+            </div>
+        <?php endif; ?>
     </div>
-    <?php if (!empty($extra_css)) { ?>
-        <style type="text/css">
-            <?php echo $extra_css; ?>
-        </style>
-    <?php } ?>
-    <script id="auth0" src="<?php echo $cdn ?>"></script>
-    <script type="text/javascript">
-        var callback = null;
+</div>
+<?php if (!empty($extra_css)): ?>
+<style type="text/css">
+    <?php echo $extra_css; ?>
+</style>
+<?php endif; ?>
+<script id="auth0" src="<?php echo $cdn ?>"></script>
+<script type="text/javascript">
+    var callback = null;
+    if(typeof(a0_wp_login) === "object"){
+        callback = a0_wp_login.initialize
+    }
 
-        if(typeof(a0_wp_login) === "object") {
-            callback = a0_wp_login.initialize
-        }
+    var widget = new Auth0Widget({
+        domain:     '<?php echo $domain; ?>',
+        chrome: true,
+        clientID:       '<?php echo $client_id; ?>',
+        callbackURL:    '<?php echo site_url('/index.php?auth0=1'); ?>',
+        container:      'auth0-login-form',
+        state:          '<?php echo $state; ?>',
+        showSignup:     <?php echo $allow_signup?'true':'false' ?>,
+        dict:           { signin: { title: '<?php echo $title ?>' } }
+    });
 
-        var lock = new Auth0Lock('<?php echo $client_id; ?>', '<?php echo $domain; ?>');
+    widget.signin({
+        onestep: true,
+        theme: 'static',
+        standalone: true,
+        showIcon: <?php echo ($show_icon ? 'true' : 'false'); ?>,
+        icon: '<?php echo ($show_icon ? WP_Auth0_Options::get('icon_url') : ''); ?>'
+    }, callback);
 
-    <?php
-
-
-        $options_obj = WP_Auth0::buildSettings(WP_Auth0_Options::get_options());
-
-        $options_obj = array_merge( array(
-            "callbackURL"   =>  site_url('/index.php?auth0=1'),
-            "authParams"    => array("state" => $state),
-        ), $options_obj  );
-
-        if (isset($specialSettings)){
-            $options_obj = array_merge( $options_obj , $specialSettings );
-        }
-
-        if (!$showAsModal) {
-            $options_obj['container'] = 'auth0-login-form';
-        }
-
-        $options = json_encode($options_obj);
-    ?>
-        function a0ShowLoginModal() {
-            var options = <?php echo $options; ?>;
-
-        <?php if ($allow_signup) { ?>
-            lock.show(options, callback);
-        <?php } else { ?>
-            lock.showSignin(options, callback);
-        <?php } ?>
-        }
-
-    <?php if (!$showAsModal) { ?>
-        a0ShowLoginModal();
-    <?php } ?>
-
-    </script>
+</script>
 <?php
-}
-?>
+endif;
