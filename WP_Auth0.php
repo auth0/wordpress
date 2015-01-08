4bd5180d36fbb409938d5ed17f7ecf97b532a48a
Sandrino Di Mattia <contact@sandrino.be>
2015-01-08 02:01:57 +0100

    Fixed Wordpress login when Auto login is enabled.

--- WP_Auth0.php
+++ WP_Auth0.php
@@ -139,7 +139,7 @@ class WP_Auth0 {
     public static function login_auto() {
         $auto_login = absint(WP_Auth0_Options::get( 'auto_login' ));
 
-        if ($auto_login && $_GET["action"] != "logout") {
+        if ($auto_login && $_GET["action"] != "logout" && !isset($_GET['wle'])) {
 
             $stateObj = array("interim" => false, "uuid" =>uniqid());
             if (isset($_GET['redirect_to'])) {
