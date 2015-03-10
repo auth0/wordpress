e8f6c164681418cf81938af094b93bd6ee0574f2
Germán Lena <german.lena@gmail.com>
2015-03-09 21:34:37 -0300

    Updated plugin version

--- WP_Auth0.php
+++ WP_Auth0.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Wordpress Auth0 Integration
  * Description: Implements the Auth0 Single Sign On solution into Wordpress
- * Version: 1.1.7
+ * Version: 1.1.8
  * Author: Auth0
  * Author URI: https://auth0.com
  */
