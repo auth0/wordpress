44a791905885b012d2863d2ab58981c1f35b42a7
German Lena <german.lena@gmail.com>
2015-06-22 18:35:43 -0300

    added style to map

--- lib/dashboard-widgets/WP_Auth0_Dashboard_Plugins_Location.php
+++ lib/dashboard-widgets/WP_Auth0_Dashboard_Plugins_Location.php
@@ -59,6 +59,8 @@ class WP_Auth0_Dashboard_Plugins_Location implements WP_Auth0_Dashboard_Plugins_
 		          zoom: 1,
 		          center: center,
 		          streetViewControl: false,
+		          mapTypeControl:false,
+		          styles:[{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}],
 		          mapTypeId: google.maps.MapTypeId.ROADMAP
 		        });
 
