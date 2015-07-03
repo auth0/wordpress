72a2decf7e887fcaf6ff11611bdc1cfae7ed3a2e
German Lena <german.lena@gmail.com>
2015-07-03 14:11:16 -0300

    changed the income chart to show the data of the user profile

--- lib/dashboard-widgets/WP_Auth0_Dashboard_Plugins_Income.php
+++ lib/dashboard-widgets/WP_Auth0_Dashboard_Plugins_Income.php
@@ -20,18 +20,40 @@ class WP_Auth0_Dashboard_Plugins_Income {
         $data = array();
 
         foreach ($this->users as $user) {
-
+            $geoip = null;
             if (isset($user->app_metadata) && isset($user->app_metadata->geoip)) {
-                if (isset($user->app_metadata->geoip->postal_code)){
-                    $postal_code = $user->app_metadata->geoip->postal_code;
-                    $country_name = $user->app_metadata->geoip->country_name;
-                    $country_code = $user->app_metadata->geoip->country_code;
+                $geoip = $user->app_metadata->geoip;
+            }elseif (isset($user->user_metadata) && isset($user->user_metadata->geoip)) {
+                $geoip = $user->user_metadata->geoip;
+            }
+
+            if ($geoip) {
+                if (isset($geoip->postal_code)){
+
+                    $postal_code = $geoip->postal_code;
+                    $country_name = $geoip->country_name;
+                    $country_code = $geoip->country_code;
+                    $income = null;
+                    if (isset($user->app_metadata->zipcode_income))
+                    {
+                        $income = $user->app_metadata->zipcode_income;
+                    }
+                    elseif (isset($user->user_metadata->zipcode_income))
+                    {
+                        $income = $user->user_metadata->zipcode_income;
+                    }
 
                     $key = "$country_name - $postal_code";
 
-                    if (!isset($data[$key])) $data[$key] = array('postal_code' => $postal_code, 'country' => $country_name, 'country_code' => $country_code, 'count' => 0);
+                    if (!isset($data[$key])) {
+                        $data[$key] = array('postal_code' => $postal_code, 'country' => $country_name, 'income' => $income, 'country_code' => $country_code, 'count' => 0);
+                    }
 
                     $data[$key]['count']++;
+                    if ($income && $data[$key]['income'] != $income)
+                    {
+                        $data[$key]['income'] = $income;
+                    }
                 }
             }
 
@@ -62,15 +84,8 @@ class WP_Auth0_Dashboard_Plugins_Income {
 
             var data = <?php echo $jsonData; ?>;
 
-            jQuery.ajax({
-                        url:'http://assets.auth0.com/zip-income/agizip.json',
-                        dataType:'json',
-                        success:function(incomes){
-                            loadChart(data, incomes);
-                        }
-                    })
-
-            function loadChart(data, incomes) {
+            loadChart(data);
+            function loadChart(data) {
 
                 var x_arr = ['x'];
                 var zipcodes_arr = ['zipcodes'];
@@ -78,7 +93,7 @@ class WP_Auth0_Dashboard_Plugins_Income {
 
                 data.forEach(function(d){
                     zipcodes_arr.push(d.count)
-                    incomes_arr.push( (d.postal_code && d.country_code == 'US' && incomes[d.postal_code.toString()]) ? incomes[d.postal_code.toString()]/100000 : 0)
+                    incomes_arr.push( d.income ? d.income/100000 : 0)
                     x_arr.push(d.country + ' - ' +d.postal_code)
                 });
 
