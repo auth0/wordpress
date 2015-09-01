b06d81afbe6f0cf6900297d37ff4f65e7f2347b1
German Lena <german.lena@gmail.com>
2015-09-01 10:59:25 -0300

    Update wording for quickstart #96

--- templates/initial-setup-step1.php
+++ templates/initial-setup-step1.php
@@ -1,20 +1,31 @@
 <div class="wrap">
 
 	<?php screen_icon(); ?>
-	<h2><?php _e('Auth0 - Initial setup', WPA0_LANG); ?></h2>
+	<h2><?php _e('Auth0 for WordPress - Quick Start Guide', WPA0_LANG); ?></h2>
 
 
-		<p>The plugin needs to get access to your Auth0 account in order to help you configure it and sincronize the profiles and his wizard will guide you through the initial setup of the Auth0 client.</p>
+		<p>This wizard will guide you through the initial setup of the Auth0 for WordPress plugin.</p>
 
-	    <p>The first step is to create an account in <a href="http://auth0.com" target="_blank">Auth0</a> if you don't have one.</p>
-	    <p>Then you need to authorize the plugin to you account in order to set up the app. The scopes required by the plugin:</p>
+    <p>Auth0 for WordPress integrates with the Auth0 cloud-based authentication service, giving you and your users:</p>
 
 		<ul class="auth0">
-	        <li><b>Read your connections</b> this will be used to sync your Auth0 connections status.</li>
-	        <li><b>Create a client (Auth0 app)</b> this will be used to create the Auth0 app used by the plugin to authenticate users and manage your account (like enable/disable rules and connections from the WordPress admin page).</li>
-	    </ul>
+			<li>A modern, easy to use login box with all the features users have come to expect.</li>
+			<li>Easy integration with 30+ social identity providers, enterprise SSO, and secure user/password authentication.</li>
+			<li>Better security including multifactor authentication, passwordless authentication, password policies, all battle-tested.</li>
+			<li>Stats on your users - demographics, locations, history.</li>
+			<li>Super easy install - 5 minutes to a better login experience for users and powerful analytics for site owners.</li>
+			<li>Customization and easy to use rules that give developers great flexibility.</li>
+    </ul>
 
-		<p><i><b>Note:</b> This plugin will call the Auth0 APIs in order to manage your accout settings. If the server is behing a firewall with restricted internet access, please whitelist the request directed to <b>*.auth0.com</b>. If this is not posible, you can manage your account using the <a href="https://manage.auth0.com">Auth0 dashboard</a></i>.</p>
+		<p>For more information about this plugin, please see the <a href="https://auth0.com/docs/wordpress">documentation</a>. For more about the Auth0 service, please visit <a href="https://auth0.com">auth0.com</a>.</p>
+
+
+		<p><i><b>IMPORTANT:</b>This plugin replaces the standard WordPress login flow. Before you configure it, please be sure that is what what you're expecting! By default, the plugin will show a link to the old login page.</i></p>
+
+		<p>Because Auth0 is a cloud service, you'll need an account at Auth0 where you'll access many of its advanced features and set things up. If you already have an Auth0 account, your WordPress installation will be one of the applications authorized in your account. If you don't have an Auth0 account, this wizard will create one for you and add your site to it all from within WordPress.</p>
+
+		<p>Once set up, most of what you'll need to do can be handled from the plugin's settings page. The plugin calls the Auth0 APIs in order to configure account settings. If your site is behind a firewall that restricts access, you will need to whitelist https requests to *.auth0.com.</p>
+		<p>If this isn't possible or desirable, or if you don't want to grant administrator privileges to one of the accounts managed by Auth0, then you can still manage your account manually using the <a href="https://manage.auth0.com/">Auth0 Dashboard</a>.</p>
 
 		<div class="auth0-btn-container">
 		    <a href="<?php echo $consent_url; ?>" class="button button-primary">Click here to start</a>
