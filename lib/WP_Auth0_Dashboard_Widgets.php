2779e3686e8e6d1977cea7f1b4f3a2bda88650b4
German Lena <german.lena@gmail.com>
2015-09-15 17:54:52 -0300

    added message explaining how to filter

--- lib/WP_Auth0_Dashboard_Widgets.php
+++ lib/WP_Auth0_Dashboard_Widgets.php
@@ -15,6 +15,33 @@ class WP_Auth0_Dashboard_Widgets  {
 	public function init() {
 		add_action( 'wp_dashboard_setup', array( $this, 'set_up' ) );
 	  add_action( 'admin_footer', array($this,'render') );
+
+		add_action('admin_notices', array($this,'show_admin_notice'));
+		add_action('admin_init', array($this,'notice_ignore'));
+	}
+
+	public function show_admin_notice() {
+		$screen = get_current_screen();
+		if ($screen->id !== 'dashboard') {
+			return;
+		}
+		
+		global $current_user ;
+    $user_id = $current_user->ID;
+
+		if ( ! get_user_meta($user_id, 'a0_ignore_widgets_explanation') ) {
+	        echo '<div class="updated"><p>';
+	        printf(__('Auth0 tip: You can filter the data clicking on the charts. Click again to clear the selection.  | <a href="%1$s">Hide</a>'), '?a0_ignore_widgets_explanation=0');
+	        echo "</p></div>";
+		}
+	}
+
+	public function notice_ignore() {
+		global $current_user;
+	  $user_id = $current_user->ID;
+    if ( isset($_GET['a0_ignore_widgets_explanation']) && '0' == $_GET['a0_ignore_widgets_explanation'] ) {
+         add_user_meta($user_id, 'a0_ignore_widgets_explanation', 'true', true);
+		}
 	}
 
 	protected function get_buckets($from, $to, $step) {
