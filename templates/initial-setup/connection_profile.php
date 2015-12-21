7aa805c1e08e1f75310a30a65944757357f6b3d2
German Lena <german.lena@gmail.com>
2015-12-21 12:34:28 -0300

    step 1

--- templates/initial-setup/connection_profile.php
+++ templates/initial-setup/connection_profile.php
@@ -5,18 +5,18 @@
   <div class="container-fluid">
     <h1><?php _e("Step 1: Choose your account type", WPA0_LANG); ?></h1>
 
-    <p class="a0-step-text"><?php _e("Users can log in witih their own credentials - social like Google or Facebook, or name/password -  or use their employee credentials through an enterprise connection. Use either or both, and you'll increase your WordPress site's security and gather data about your visitors.", WPA0_LANG); ?></p>
+    <p class="a0-step-text"><?php _e("Users can log in within their own credentials - social like Google or Facebook, or name/password -  or use their employee credentials through an enterprise connection. Use either or both, and you'll increase your WordPress site's security and gather data about your visitors.", WPA0_LANG); ?></p>
 
     <div class="a0-profiles row">
       <form action="options.php" method="POST">
 
         <input type="hidden" name="action" value="wpauth0_callback_step2" />
 
-        <div class="col col-sm-6">
+        <div class="col col-sm-offset-1 col-sm-5">
           <div class="profile">
             <img src="<?php echo WPA0_PLUGIN_URL; ?>/assets/img/initial-setup/simple-end-users-login.svg">
 
-            <h2><?php _e("Simple end-user logins.", WPA0_LANG); ?></h2>
+            <h2><?php _e("Simple end-user logins", WPA0_LANG); ?></h2>
 
             <p><?php _e("With this option, your users can log in with popular social accounts like Google or Facebook, or choose their own username and password.", WPA0_LANG); ?></p>
             
@@ -26,11 +26,11 @@
           </div>
         </div>
 
-        <div class="col col-sm-6">
+        <div class="col col-sm-5">
           <div class="profile">
             <img src="<?php echo WPA0_PLUGIN_URL; ?>/assets/img/initial-setup/effortless-employee-access.svg">
 
-            <h2><?php _e("Effortless employee access.", WPA0_LANG); ?></h2>
+            <h2><?php _e("Effortless employee access", WPA0_LANG); ?></h2>
 
             <p><?php _e("Let users log in with their work credentials by connecting your WordPress instance with your enterprise directory through Auth0. Auth0 will create a user record for each such user in a private database.", WPA0_LANG); ?></p>
 
@@ -47,7 +47,7 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <h4 class="modal-title" id="myModalLabel">Important</h4>
               </div>
               <div class="modal-body">
                 <p><?php _e('This wizard gets you started with the Auth0 for WordPress plug-in. You\'ll be transferred to Auth0 and can login or sign-up. Then you\'ll authorize the plug-in and configure identity providers, whether social or enterprise connections.', WPA0_LANG); ?></p>
