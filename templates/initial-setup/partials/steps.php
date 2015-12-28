cf39acbca25548039a8073088df97eaa47675d08
German Lena <german.lena@gmail.com>
2015-12-28 14:23:50 -0300

    fixes mobile layout

--- templates/initial-setup/partials/steps.php
+++ templates/initial-setup/partials/steps.php
@@ -3,8 +3,8 @@
 
   <div class="steps row">
     
-    <div class="step col-xs-offset-3 col-xs-3 <?php echo ($step ==2 ? 'active' : ''); ?>"><span>1</span><p><?php echo _e('Setup Social Single Sign On'); ?></p></div>
-    <div class="step col-xs-3 <?php echo ($step ==3 ? 'active' : ''); ?>"><span>2</span><p><?php echo _e('Migrate your administrator account'); ?></p></div>
+    <div class="step col-xs-offset-2 col-xs-4 col-sm-offset-3 col-sm-3 <?php echo ($step ==2 ? 'active' : ''); ?>"><span>1</span><p><?php echo _e('Setup Social Single Sign On'); ?></p></div>
+    <div class="step col-xs-4 col-sm-3 <?php echo ($step ==3 ? 'active' : ''); ?>"><span>2</span><p><?php echo _e('Migrate your administrator account'); ?></p></div>
 
   </div>
 
