e2e6a8617c248e22db046f4e23310c3bbcf3f296
German Lena <german.lena@gmail.com>
2016-01-05 10:24:50 -0300

    import/export layout

--- templates/import_settings.php
+++ templates/import_settings.php
@@ -1,10 +1,8 @@
-<div class="a0-wrap">
+<div class="a0-wrap settings">
 
   <?php require(WPA0_PLUGIN_DIR . 'templates/initial-setup/partials/header.php'); ?>
 
   <div class="container-fluid">
-
-  	<h1><?php _e('Import/Export Settings', WPA0_LANG); ?></h1>
 
     <div class="row">
   	 
@@ -13,12 +11,16 @@
         <form action="options.php" method="post" onsubmit="return presubmit_import();" enctype="multipart/form-data">
           <input type="hidden" name="action" value="wpauth0_import_settings" />
     			
-          <h2>Import settings</h2>
+          <h1><?php _e('Import Settings', WPA0_LANG); ?></h1>
     			
-          <div>Upload a file:</div>
-          <div><input type="file" name="settings-file" /></div>
-          <div>or copy the json</div>
-    			<div><textarea name="settings-json"></textarea></div>
+          <div id="upload-file">
+            <p class="a0-step-text">Please upload the exported json file or <span class="link upload-toggle">paste the entire json</span>.</p>
+            <input type="file" name="settings-file" />
+          </div>
+    			<div id="paste-json" style="display:none;">
+            <p class="a0-step-text">Please paste the exported json file or <span class="link upload-toggle">upload the exported file</span>.</p>
+            <textarea name="settings-json"></textarea>
+          </div>
 
           <div class="a0-buttons">          
             <input type="submit" name="setup" class="a0-button primary" value="Import" />
@@ -33,7 +35,9 @@
         <form action="options.php" method="post" onsubmit="return presubmit_export();">
           <input type="hidden" name="action" value="wpauth0_export_settings" />
 
-    			<h2>Export settings</h2>
+          <h1><?php _e('Export Settings', WPA0_LANG); ?></h1>
+
+          <p class="a0-step-text">Download the entire plugin configuration.</p>
 
           <div class="a0-buttons">          
             <input type="submit" name="setup" class="a0-button primary" value="Export" />
@@ -51,6 +55,12 @@
 
 
 <script type="text/javascript">
+  jQuery('.upload-toggle').click(function(){
+
+    jQuery('#upload-file').toggle();
+    jQuery('#paste-json').toggle();
+
+  });
   function presubmit_import() {
     if (typeof(a0metricsLib) !== 'undefined') {
       a0metricsLib.track('import:settings', {});
