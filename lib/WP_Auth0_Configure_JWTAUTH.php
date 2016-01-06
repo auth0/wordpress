c12d69556cb132fd6bbbbc7b2ae2fd30d663df9b
German Lena <german.lena@gmail.com>
2016-01-06 09:53:08 -0300

    jwt setup layout

--- lib/WP_Auth0_Configure_JWTAUTH.php
+++ lib/WP_Auth0_Configure_JWTAUTH.php
@@ -37,6 +37,10 @@ class WP_Auth0_Configure_JWTAUTH {
 			return;
 		}
 		wp_enqueue_media();
+    wp_enqueue_style( 'wpa0_bootstrap', WPA0_PLUGIN_URL . 'assets/bootstrap/css/bootstrap.min.css' );
+    wp_enqueue_script( 'wpa0_bootstrap', WPA0_PLUGIN_URL . 'assets/bootstrap/js/bootstrap.min.js' );
+    wp_enqueue_style( 'wpa0_admin_initial_settup', WPA0_PLUGIN_URL . 'assets/css/initial-setup.css' );
+    wp_enqueue_style( 'media' );
 	}
 
 	public function render_settings_page() {
--- templates/configure-jwt-auth.php
+++ templates/configure-jwt-auth.php
@@ -1,17 +1,22 @@
-<div class="wrap">
-	<?php screen_icon(); ?>
-	<h2><?php _e('JWT Auth authentication', WPA0_LANG); ?></h2>
+<div class="a0-wrap">
 
-	<div class="container">
-	<?php if (!$ready) { ?>
-		<form action="options.php" method="post">
-			<input type="hidden" name="action" value="wpauth0_configure_jwt" />
-			<p>This action will override the JWT Auth configuration and will enable it to authenticate users using the Auth0 JWT.</p>
-			<p>Do you want to continue?</p>
-			<p><input type="submit" name="setup" value="Yes" class="button button-primary"/></p>
-		</form>
-	<?php } else { ?>
-		<p>JWT is configured and ready to work with Auth0 tokens.</p>
-	<?php } ?>
+	<?php require(WPA0_PLUGIN_DIR . 'templates/initial-setup/partials/header.php'); ?>
+
+	<div class="container-fluid">
+
+		<h1><?php _e('JWT Auth authentication', WPA0_LANG); ?></h1>
+
+		<?php if (!$ready) { ?>
+			<form action="options.php" method="post">
+				<input type="hidden" name="action" value="wpauth0_configure_jwt" />
+				<p class="a0-step-text">This action will override the JWT Auth configuration and will enable it to authenticate users using the Auth0 JWT.<br>Do you want to continue?</p>
+
+				<div class="a0-buttons">			    
+					<input type="submit" name="submit" id="submit" class="a0-button primary" value="Yes" />
+				</div>
+			</form>
+		<?php } else { ?>
+			<p class="a0-step-text">JWT is configured and ready to work with Auth0 tokens.</p>
+		<?php } ?>
 	</div>
 </div>
