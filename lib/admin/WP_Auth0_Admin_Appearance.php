1c4884caffcac630a0cdbed58d185d2f93ec3092
German Lena <german.lena@gmail.com>
2016-02-02 12:14:10 -0300

    https://github.com/auth0/wp-auth0/issues/133

--- lib/admin/WP_Auth0_Admin_Appearance.php
+++ lib/admin/WP_Auth0_Admin_Appearance.php
@@ -91,7 +91,7 @@ class WP_Auth0_Admin_Appearance extends WP_Auth0_Admin_Generic {
       <div class="subelement">
         <span class="description">
           <?php echo __( 'If you don\'t want to validate that the user enters an email, just set this to username.', WPA0_LANG ); ?>
-          <a target="_blank" href="https://github.com/auth0/lock/wiki/Auth0Lock-customization#usernamestyle-string"><?php echo __( 'More info', WPA0_LANG ); ?></a>
+          <a target="_blank" href="https://auth0.com/docs/libraries/lock/customization#usernamestyle-string-"><?php echo __( 'More info', WPA0_LANG ); ?></a>
         </span>
       </div>
     <?php
