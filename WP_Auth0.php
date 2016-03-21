2305042fe6519261c70958f6f897658ace1e502c
German Lena <german.lena@gmail.com>
2016-03-21 13:44:54 -0300

    Plugin description text #155

--- WP_Auth0.php
+++ WP_Auth0.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Auth0 for WordPress
- * Description: Implements the Auth0 Single Sign On solution into Wordpress
+ * Description: Let your users login with any social provider from Twitter to Facebook as well as many others like Github. Enterprise? We've got you covered - <a href="https://auth0.com">Auth0</a> is a full enterprise solution and will work with your Active Directory or any other environment to take control of your wordpress sites.
  * Version: 2.1.4
  * Author: Auth0
  * Author URI: https://auth0.com
