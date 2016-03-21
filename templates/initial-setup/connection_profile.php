64049d64cd0815725486826e2fc388d906b6d971
German Lena <german.lena@gmail.com>
2016-03-21 16:11:47 -0300

    auth0 logo on the quick start guide #173

--- templates/initial-setup/connection_profile.php
+++ templates/initial-setup/connection_profile.php
@@ -5,7 +5,7 @@
   <div class="container-fluid">
     <div class="row">
       <h1><?php _e("Step 1: Choose your account type", WPA0_LANG); ?></h1>
-      <p class="a0-step-text"><?php _e("Users can log in within their own credentials - social like Google or Facebook, or name/password -  or use their employee credentials through an enterprise connection. Use either or both, and you'll increase your WordPress site's security and gather data about your visitors.", WPA0_LANG); ?></p>
+      <p class="a0-step-text"><?php _e("Users can log in within their own credentials - social like Google or Facebook, or name/password -  or use their employee credentials through an enterprise connection. Use either or both, and you'll increase your WordPress site's security and gather data about your visitors. For more information and help on the Auth0 WordPress plugin please visit", WPA0_LANG); ?> <a href="https://auth0.com/docs/cms" target="_blank">our help documentation</a> </p>
     </div>
     <div class="a0-profiles row">
       <form action="options.php" method="POST" id="profile-form">
