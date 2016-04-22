c2d12d64981cf3e1db726d7a36056817d36d384f
German Lena <german.lena@gmail.com>
2016-04-22 11:18:33 -0300

    auth0 account creation #192 + new plugins info

--- templates/initial-setup/signup.php
+++ templates/initial-setup/signup.php
@@ -4,8 +4,8 @@
 
   <div class="container-fluid">
     <div class="row">
-      <h1><?php _e("Create your Auth0 account", WPA0_LANG); ?></h1>
-      <p class="a0-step-text"><?php _e("Users can log in within their own credentials - social like Google or Facebook, or name/password -  or use their employee credentials through an enterprise connection. Use either or both, and you'll increase your WordPress site's security and gather data about your visitors. For more information and help on the Auth0 WordPress plugin please visit", WPA0_LANG); ?> <a href="https://auth0.com/docs/cms" target="_blank">our help documentation</a> </p>
+      <h1><?php _e("Sign Up with Auth0", WPA0_LANG); ?></h1>
+      <p class="a0-step-text"><?php _e("To get started with the plugin we need you to sign up with Auth0 below. It should only take a second:", WPA0_LANG); ?></p>
     </div>
   </div>
 </div>
