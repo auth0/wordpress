b49816ccac9e6c94d900f398057c472bd38de11b
German Lena <german.lena@gmail.com>
2016-06-01 09:50:27 -0300

    removed todo file

--- lib/TODO.php
+++
@@ -1,5 +0,0 @@
-
-one profile per user (go back with users link, update the rule to merge metadata)
-change how it gets the profile, now comes from wp_user metadata
-
-migrate data from table to meta
