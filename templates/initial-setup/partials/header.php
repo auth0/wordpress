c5c1d15f98b68917a30f971987463a0665a88813
Rudy Affandi <rudy@adnetinc.com>
2016-07-27 14:30:44 -0300

    Force logo to https
    Otherwise if accessed from an https page, it will complain re: mixed content

--- templates/initial-setup/partials/header.php
+++ templates/initial-setup/partials/header.php
@@ -1,5 +1,5 @@
 <div class="a0-header">
   <a href="https://manage.auth0.com" target="_blank">
-    <img class="a0-header-logo" style="height: 55px;" src="http://cdn.auth0.com/styleguide/latest/img/logo-blue.png" />
+    <img class="a0-header-logo" style="height: 55px;" src="https://cdn.auth0.com/styleguide/latest/img/logo-blue.png" />
   </a>
 </div>
