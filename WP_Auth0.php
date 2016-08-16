63b306e6fbf81000c04bc792c6e324cdedd9e57f
German Lena <german.lena@gmail.com>
2016-08-16 15:38:14 -0300

    bump

--- WP_Auth0.php
+++ WP_Auth0.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PLUGIN_NAME
  * Description: PLUGIN_DESCRIPTION
- * Version: 3.2.1
+ * Version: 3.2.2
  * Author: Auth0
  * Author URI: https://auth0.com
  */
@@ -12,7 +12,7 @@ define( 'WPA0_PLUGIN_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'WPA0_PLUGIN_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'WPA0_LANG', 'wp-auth0' );
 define( 'AUTH0_DB_VERSION', 10 );
-define( 'WPA0_VERSION', '3.2.1' );
+define( 'WPA0_VERSION', '3.2.2' );
 
 /**
  * Main plugin class
