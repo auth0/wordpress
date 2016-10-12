8da28ce5b7cb8506a071db88677834fdeabe2a0e
German Lena <german.lena@gmail.com>
2016-10-12 13:53:46 -0300

    disable autoprovision by default and only honor the signup flag

--- lib/WP_Auth0_Options.php
+++ lib/WP_Auth0_Options.php
@@ -112,7 +112,7 @@ class WP_Auth0_Options extends WP_Auth0_Options_Generic {
 
 			'force_https_callback' => false,
 			
-			'auto_provisioning' => true,
+			'auto_provisioning' => false,
 			'default_login_redirection' => home_url(),
 
 			'auth0_server_domain' => 'auth0.auth0.com',
