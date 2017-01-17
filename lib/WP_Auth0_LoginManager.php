0bb2694576fd6a306af9208325e6b30b3215fe3c
Carlos Mostek <carlos.mostek@auth0.com>
2017-01-17 14:01:53 -0600

    Use get_user instead of user_info to avoid missing identities

--- lib/WP_Auth0_LoginManager.php
+++ lib/WP_Auth0_LoginManager.php
@@ -240,8 +240,18 @@ class WP_Auth0_LoginManager {
 
 			if ( !isset( $data->id_token ) ) {
 				$data->id_token = null;
+				$response = WP_Auth0_Api_Client::get_user_info( $domain, $data->access_token );
+			} else {
+				// grab the user ID from the id_token to call get_user
+				$decodedToken = JWT::decode( $data->id_token, $client_secret, array( 'HS256' ) );
+
+				// validate that this JWT was made for us
+				if ( $this->a0_options->get( 'client_id' ) !== $decodedToken->aud ) {
+					throw new Exception( 'This token is not intended for us.' );
+				}
+
+				$response = WP_Auth0_Api_Client::get_user( $domain, $data->id_token, $decodedToken->sub );
 			}
-			$response = WP_Auth0_Api_Client::get_user_info( $domain, $data->access_token );
 
 			if ( $response instanceof WP_Error ) {
 				WP_Auth0_ErrorManager::insert_auth0_error( 'init_auth0_userinfo', $response );
