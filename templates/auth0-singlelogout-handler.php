d6ad76bab4d26a5a7c91a9dd75b4d897ebd90904
Cody Ayers <codyayers@users.noreply.github.com>
2017-02-14 15:20:30 -0500

    Update auth0-singlelogout-handler.php
    Adding version for Lock 9 and logic to switch between both.

--- templates/auth0-singlelogout-handler.php
+++ templates/auth0-singlelogout-handler.php
@@ -4,6 +4,7 @@
 
   var uuids = '<?php echo $user_profile->user_id; ?>';
   document.addEventListener("DOMContentLoaded", function() {
+  <?php if ($this->a0_options->get('use_lock_10')) { ?>
     var client = new Auth0({
       domain:       '<?php echo $domain; ?>',
       clientID:     '<?php echo $client_id; ?>',
@@ -12,11 +13,17 @@
 
     client.getSSOData(function(err, data) {
       if (!err && ( !data.sso || uuids != data.lastUsedUserID) ) {
-
         window.location = '<?php echo html_entity_decode( $logout_url ); ?>';
-
       }
     });
+  <?php } else { ?>
+    var lock = new Auth0Lock('<?php echo $client_id; ?>', '<?php echo $domain; ?>');
+      lock.$auth0.getSSOData(function(err, data) {
+        if (!err && ( !data.sso || uuids != data.lastUsedUserID) ) {
+          window.location = '<?php echo html_entity_decode( $logout_url ); ?>';
+        }
+      });
+  <?php } ?>
   });
 
 })();
