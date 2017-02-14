338eca69f2f6075da010bd2bb6d22dc154aa8641
Cody Ayers <codyayers@users.noreply.github.com>
2017-02-14 15:08:12 -0500

    Fixes compatibility issues with Lock 10
    Fixes compatibility issues with Lock 10 causing the following error:
    
    Uncaught TypeError: Cannot read property 'getSSOData' of undefined

--- templates/auth0-singlelogout-handler.php
+++ templates/auth0-singlelogout-handler.php
@@ -4,8 +4,13 @@
 
   var uuids = '<?php echo $user_profile->user_id; ?>';
   document.addEventListener("DOMContentLoaded", function() {
-    var lock = new Auth0Lock('<?php echo $client_id; ?>', '<?php echo $domain; ?>');
-    lock.$auth0.getSSOData(function(err, data) {
+    var client = new Auth0({
+      domain:       '<?php echo $domain; ?>',
+      clientID:     '<?php echo $client_id; ?>',
+      responseType: 'token'
+    });
+
+    client.getSSOData(function(err, data) {
       if (!err && ( !data.sso || uuids != data.lastUsedUserID) ) {
 
         window.location = '<?php echo html_entity_decode( $logout_url ); ?>';
