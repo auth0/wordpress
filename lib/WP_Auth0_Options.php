d69a3fa85f57b27ab330ff7ebb8c5c2dd32d1961
Thameera Senanayaka <thameera123@gmail.com>
2017-04-24 15:08:15 +0530

    Disable base64_encoded by default
    Since all new clients created are not base64 encoded, it does not make
    sense to keep this turned on by default.

--- lib/WP_Auth0_Options.php
+++ lib/WP_Auth0_Options.php
@@ -58,7 +58,7 @@ class WP_Auth0_Options extends WP_Auth0_Options_Generic {
 			'auto_login_method' => '',
 			'client_id' => '',
 			'client_secret' => '',
-			'client_secret_b64_encoded' => true,
+			'client_secret_b64_encoded' => false,
 			'domain' => '',
 			'form_title' => '',
 			'icon_url' => '',
