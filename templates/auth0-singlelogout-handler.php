4a1e611809c6e75846826e8e43aca927aa68dfb6
Martin Walsh <martin.walsh@gmail.com>
2017-10-25 16:07:30 +0100

    Support Single Logout SSO

--- templates/auth0-singlelogout-handler.php
+++ templates/auth0-singlelogout-handler.php
@@ -13,12 +13,17 @@
       domain:'<?php echo $domain; ?>'
     });
 
-    webAuth.client.getSSOData(function(err, data) {
-      if (!err && ( !data.sso || uuids != data.lastUsedUserID)) {
-        window.location = '<?php echo html_entity_decode( $logout_url ); ?>';
+    var options = <?php echo json_encode( $lock_options->get_sso_options() ); ?>;
+    webAuth.checkSession(options, function (err, authResult) {
+      console.log(err);
+      console.log(authResult);
+      if (err !== null) {
+        if(err.error ==='login_required') {
+          window.location = '<?php echo html_entity_decode( $logout_url ); ?>';
+        }
       }
     });
-  });
 
+  });
 })();
 </script>
