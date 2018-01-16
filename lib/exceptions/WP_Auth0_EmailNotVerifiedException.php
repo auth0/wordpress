ca056b77a6576e879c8fcb34a5b80a3872b44191
Josh Cunningham <josh@joshcanhelp.com>
2018-01-16 11:59:33 -0800

    removing deprecated doc tag in WP_Auth0_EmailNotVerifiedException

--- lib/exceptions/WP_Auth0_EmailNotVerifiedException.php
+++ lib/exceptions/WP_Auth0_EmailNotVerifiedException.php
@@ -12,9 +12,9 @@ class WP_Auth0_EmailNotVerifiedException extends Exception {
 	 * WP_Auth0_EmailNotVerifiedException constructor.
 	 *
 	 * @param stdClass $userinfo - userinfo object returned from Auth0
-	 * @param string $id_token - DEPRECATED 3.4.1, should not be output in any template
+	 * @param string $id_token - should not be output in any template
 	 */
-	public function __construct( $userinfo, $id_token = '' ) {
+	public function __construct( $userinfo, $id_token ) {
 		$this->userinfo = $userinfo;
 		$this->id_token = $id_token;
 	}
