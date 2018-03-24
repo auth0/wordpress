76ad48f67d0c025173759ab5e1b89c6eb968b8e0
Josh Cunningham <josh@joshcanhelp.com>
2018-03-24 07:24:20 -0700

    Refactored passwordless
    Passwordless (PWL) was not working with new tenants and used the old,
    separate PWL JS library. This commit updates the Lock library used to
    11.5, moves and re-writes the JS used to show Lock, loads required JS
    in one place for widget + shortcode + wp-login, and revises state
    handling to work better for the shortcode and widget.

--- templates/login-form.php
+++ templates/login-form.php
@@ -6,12 +6,32 @@ function renderAuth0Form( $canShowLegacyLogin = true, $specialSettings = array()
 	if ( !$canShowLegacyLogin || !isset( $_GET['wle'] ) ) {
     $options = WP_Auth0_Options::Instance();
 
-    if (!$options->get('passwordless_enabled')) {
+    if ( ! $options->get('passwordless_enabled', FALSE) ) {
       wp_enqueue_script( 'wpa0_lock', $options->get('cdn_url'), array( 'jquery' ), FALSE );
       require_once 'auth0-login-form-lock10.php';
     } else {
+      $lock_options = new WP_Auth0_Lock10_Options( $specialSettings );
+
+      // This is output in the footer so it can run after wp_head
       wp_enqueue_script( 'wpa0_lock', $options->get('passwordless_cdn_url'), array( 'jquery' ), FALSE );
-      require_once 'auth0-login-form.php';
+      wp_enqueue_script( 'auth0-pwl', WPA0_PLUGIN_JS_URL . 'login-pwl.js', array( 'jquery' ), WPA0_VERSION, TRUE );
+
+      // Set required global var
+      wp_localize_script(
+        'auth0-pwl',
+        'wpAuth0PwlGlobal',
+        array(
+          'i18n' => array(),
+          'lock' => array(
+            'options' => $lock_options->get_lock_options(),
+            'ready' => WP_Auth0::ready(),
+            'domain' => $options->get( 'domain' ),
+            'clientId' => $options->get( 'client_id' ),
+          ),
+        )
+      );
+
+      require_once 'auth0-login-form-pwl.php';
     }
 	} else {
 		add_action( 'login_footer', array( 'WP_Auth0', 'render_back_to_auth0' ) );
