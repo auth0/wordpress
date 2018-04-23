ee8551d6ddbc6905b607f2f1c65b502bfc7236c3
Josh Cunningham <josh@joshcanhelp.com>
2018-04-23 09:57:37 -0700

    Deprecating admin_enqueue functions
    - WP_Auth0_InitialSetup::admin_enqueue()
    - WP_Auth0_Configure_JWTAUTH::admin_enqueue()
    - WP_Auth0_ErrorLog::init()
    - WP_Auth0_ErrorLog::admin_enqueue()
    - WP_Auth0_Export_Users::admin_enqueue()

--- lib/WP_Auth0_Configure_JWTAUTH.php
+++ lib/WP_Auth0_Configure_JWTAUTH.php
@@ -31,16 +31,12 @@ class WP_Auth0_Configure_JWTAUTH {
 		<?php
 	}
 
-	// TODO: Deprecate
+	/**
+	 * @deprecated 3.6.0 - Not needed, handled in WP_Auth0_Admin::admin_enqueue()
+	 */
 	public function admin_enqueue() {
-		if ( ! isset( $_REQUEST['page'] ) || 'wpa0-jwt-auth' !== $_REQUEST['page'] ) {
-			return;
-		}
-		wp_enqueue_media();
-		wp_enqueue_style( 'wpa0_bootstrap', WPA0_PLUGIN_URL . 'assets/bootstrap/css/bootstrap.min.css' );
-		wp_enqueue_script( 'wpa0_bootstrap', WPA0_PLUGIN_URL . 'assets/bootstrap/js/bootstrap.min.js' );
-		wp_enqueue_style( 'wpa0_admin_initial_settup', WPA0_PLUGIN_URL . 'assets/css/initial-setup.css' );
-		wp_enqueue_style( 'media' );
+		// phpcs:ignore
+		trigger_error( sprintf( __( 'Method %s is deprecated.', 'wp-auth0' ), __METHOD__ ), E_USER_DEPRECATED );
 	}
 
 	public function render_settings_page() {
