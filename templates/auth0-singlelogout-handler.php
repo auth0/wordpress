c90de85016b6d9d1f8cd0f18fae6faec7f837e61
Josh Cunningham <josh@joshcanhelp.com>
2018-05-22 14:21:18 -0700

    Fixing SLO redirect and SSO setting push to dashboard

--- templates/auth0-singlelogout-handler.php
+++ templates/auth0-singlelogout-handler.php
@@ -3,6 +3,11 @@ $current_user = get_currentauth0user();
 if ( empty( $current_user->auth0_obj ) ) {
   return;
 }
+
+$logout_url = wp_logout_url();
+$logout_url = html_entity_decode( $logout_url );
+$logout_url = add_query_arg( 'redirect_to', get_permalink(), $logout_url );
+$logout_url = add_query_arg( 'SLO', 1, $logout_url );
 ?>
 <script id="auth0" src="<?php echo esc_url( $this->a0_options->get('auth0js-cdn') ) ?>"></script>
 <script type="text/javascript">
@@ -18,9 +23,10 @@ if ( empty( $current_user->auth0_obj ) ) {
       domain:'<?php echo sanitize_text_field( $this->a0_options->get( 'domain' ) ); ?>'
     });
 
-    webAuth.checkSession( { 'responseType' : 'token', 'redirectUri' : window.location.href }, function ( err ) {
-            if ( err && err.error ==='login_required' ) {
-                window.location = '<?php echo esc_url( wp_logout_url( get_permalink() ) . '&SLO=1' ); ?>';
+    var sloOptions = { 'responseType' : 'token id_token', 'redirectUri' : '<?php echo home_url() ?>' };
+    webAuth.checkSession( sloOptions, function ( err ) {
+            if ( err && err.error && 'login_required' === err.error ) {
+                window.location = '<?php echo $logout_url ?>';
             }
         }
     );
