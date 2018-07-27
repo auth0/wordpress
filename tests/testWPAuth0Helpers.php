23e44c2f25b5be5d96f0bf35d79235d758cb2f6d
Josh Cunningham <josh@joshcanhelp.com>
2018-07-27 10:32:53 -0700

    Refactor tenant region to use regex + tests; fix js error

--- tests/testWPAuth0Helpers.php
+++ tests/testWPAuth0Helpers.php
@@ -14,13 +14,17 @@ class TestWPAuth0Helpers extends TestCase {
 		// Test the default.
 		$this->assertEquals( 'us', WP_Auth0::get_tenant_region( 'banana' ) );
 		$this->assertEquals( 'us', WP_Auth0::get_tenant_region( 'banana.auth0.com' ) );
+		$this->assertEquals( 'us', WP_Auth0::get_tenant_region( 'banana.us.auth0.com' ) );
 		$this->assertEquals( 'eu', WP_Auth0::get_tenant_region( 'apple.eu.auth0.com' ) );
 		$this->assertEquals( 'au', WP_Auth0::get_tenant_region( 'orange.au.auth0.com' ) );
+		$this->assertEquals( 'xx', WP_Auth0::get_tenant_region( 'mango.xx.auth0.com' ) );
 
 		// Test full tenant name getting.
 		$this->assertEquals( 'banana@us', WP_Auth0::get_tenant( 'banana' ) );
 		$this->assertEquals( 'banana@us', WP_Auth0::get_tenant( 'banana.auth0.com' ) );
+		$this->assertEquals( 'banana@us', WP_Auth0::get_tenant( 'banana.us.auth0.com' ) );
 		$this->assertEquals( 'apple@eu', WP_Auth0::get_tenant( 'apple.eu.auth0.com' ) );
 		$this->assertEquals( 'orange@au', WP_Auth0::get_tenant( 'orange.au.auth0.com' ) );
+		$this->assertEquals( 'mango@xx', WP_Auth0::get_tenant( 'mango.xx.auth0.com' ) );
 	}
 }
