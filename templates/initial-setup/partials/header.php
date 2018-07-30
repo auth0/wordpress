7ada01911c11103e4c0058489fa61243d0acca6a
Josh Cunningham <josh@joshcanhelp.com>
2018-07-30 15:21:55 -0700

    Add core settings override methods and tests

--- templates/initial-setup/partials/header.php
+++ templates/initial-setup/partials/header.php
@@ -1,3 +1,6 @@
+<?php
+// phpcs:ignoreFile
+?>
 <div class="a0-header">
   <a href="https://manage.auth0.com" target="_blank">
     <img class="a0-header-logo" style="height: 55px;" src="https://cdn.auth0.com/styleguide/latest/img/logo-blue.png" />
