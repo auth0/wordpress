aaf84c8e68ca939d6f4fe59714408f233ee927bb
Josh Cunningham <josh@joshcanhelp.com>
2018-10-12 07:59:06 -0700

    Fix tests for no processIsolation

--- tests/testLockOptions.php
+++ tests/testLockOptions.php
@@ -17,23 +17,36 @@ class TestLockOptions extends TestCase {
 	use setUpTestDb;
 
 	/**
+	 * WP_Auth0_Options instance.
+	 *
+	 * @var WP_Auth0_Options
+	 */
+	protected static $opts;
+
+	/**
+	 * Run before test suite.
+	 */
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+		self::$opts = WP_Auth0_Options::Instance();
+	}
+
+	/**
 	 * Test that a custom domain adds a correct key for CDN configuration to Lock options.
 	 */
 	public function testThatLockConfigBaseUrlIsBuiltProperly() {
-		$opts = WP_Auth0_Options::Instance();
-
-		$opts->set( 'domain', 'test.auth0.com' );
-		$lock_options     = new WP_Auth0_Lock10_Options( [], $opts );
+		self::$opts->set( 'domain', 'test.auth0.com' );
+		$lock_options     = new WP_Auth0_Lock10_Options( [], self::$opts );
 		$lock_options_arr = $lock_options->get_lock_options();
 		$this->assertArrayNotHasKey( 'configurationBaseUrl', $lock_options_arr );
 
-		$opts->set( 'custom_domain', 'login.example.com' );
+		self::$opts->set( 'custom_domain', 'login.example.com' );
 		$this->assertEquals( 'https://cdn.auth0.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
 
-		$opts->set( 'domain', 'test.eu.auth0.com' );
+		self::$opts->set( 'domain', 'test.eu.auth0.com' );
 		$this->assertEquals( 'https://cdn.eu.auth0.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
 
-		$opts->set( 'domain', 'test.au.auth0.com' );
+		self::$opts->set( 'domain', 'test.au.auth0.com' );
 		$this->assertEquals( 'https://cdn.au.auth0.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
 	}
 
@@ -41,16 +54,23 @@ class TestLockOptions extends TestCase {
 	 * Test that callback URLs are built properly
 	 */
 	public function testThatAuthCallbacksAreCorrect() {
-		$opts         = WP_Auth0_Options::Instance();
-		$lock_options = new WP_Auth0_Lock10_Options( [], $opts );
+		$lock_options = new WP_Auth0_Lock10_Options( [], self::$opts );
 
 		$this->assertEquals( 'http://example.org/index.php?auth0=implicit', $lock_options->get_implicit_callback_url() );
 		$this->assertEquals( 'http://example.org/index.php?auth0=1', $lock_options->get_code_callback_url() );
 
-		$opts->set( 'force_https_callback', 1 );
-		$lock_options = new WP_Auth0_Lock10_Options( [], $opts );
+		self::$opts->set( 'force_https_callback', 1 );
+		$lock_options = new WP_Auth0_Lock10_Options( [], self::$opts );
 
 		$this->assertEquals( 'https://example.org/index.php?auth0=implicit', $lock_options->get_implicit_callback_url() );
 		$this->assertEquals( 'https://example.org/index.php?auth0=1', $lock_options->get_code_callback_url() );
+	}
+
+	/**
+	 * Run after each test.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+		self::$opts->set( 'force_https_callback', null );
 	}
 }
