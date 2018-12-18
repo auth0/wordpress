866352ac9d26c3e924ccd7dd2ee356a86903f8d1
Josh Cunningham <josh@joshcanhelp.com>
2018-12-17 20:40:55 -0800

    Refactor mogration route handling and add tests.

--- tests/testRoutes.php
+++ tests/testRoutes.php
@@ -11,21 +11,20 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * Class TestRoutes.
+ *
+ * @group routes
  */
 class TestRoutes extends TestCase {
 
 	use HookHelpers;
+
+	use OptionsHelpers;
 
 	use SetUpTestDb {
 		setUp as setUpDb;
 	}
 
 	use UsersHelper;
-
-	/**
-	 * Default query_vars state.
-	 */
-	const WP_OBJECT_DEFAULT = [ 'query_vars' => [ 'custom_requests_return' => true ] ];
 
 	/**
 	 * Instance of WP_Auth0_Options.
@@ -51,7 +50,7 @@ class TestRoutes extends TestCase {
 	/**
 	 * Mock WP instance.
 	 *
-	 * @var stdClass|WP_Query
+	 * @var WP
 	 */
 	protected static $wp;
 
@@ -64,7 +63,6 @@ class TestRoutes extends TestCase {
 		self::$routes = new WP_Auth0_Routes( self::$opts );
 
 		self::$error_log = new WP_Auth0_ErrorLog();
-		self::$wp        = (object) self::WP_OBJECT_DEFAULT;
 	}
 
 	/**
@@ -73,7 +71,9 @@ class TestRoutes extends TestCase {
 	public function setUp() {
 		parent::setUp();
 		$this->setUpDb();
-		self::$wp = (object) self::WP_OBJECT_DEFAULT;
+		self::$wp = new WP();
+		self::$wp->set_query_var( 'custom_requests_return', true );
+		self::$opts->reset();
 	}
 
 	/**
@@ -103,5 +103,48 @@ class TestRoutes extends TestCase {
 		$this->assertFalse( self::$routes->custom_requests( self::$wp ) );
 
 		$this->assertEmpty( self::$error_log->get() );
+	}
+
+	/**
+	 * Test that the OAuth configuration returns the correct values.
+	 */
+	public function testThatOauthConfigIsCorrect() {
+		self::$wp->set_query_var( 'a0_action', 'oauth2-config' );
+
+		$output = json_decode( self::$routes->custom_requests( self::$wp ), true );
+
+		$this->assertEquals( 'Test Blog', $output['client_name'] );
+		$this->assertCount( 1, $output['redirect_uris'] );
+		$this->assertEquals(
+			'http://example.org/wp-admin/admin.php?page=wpa0-setup&callback=1',
+			$output['redirect_uris'][0]
+		);
+
+		self::$wp->set_query_var( 'a0_action', null );
+		self::$wp->set_query_var( 'pagename', 'oauth2-config' );
+		$output_2 = json_decode( self::$routes->custom_requests( self::$wp ), true );
+		$this->assertEquals( $output, $output_2 );
+	}
+
+	/**
+	 * Test that the COO fallback outputs the correct values.
+	 */
+	public function testThatCooFallbackIsCorrect() {
+		self::auth0Ready( true );
+		self::$wp->set_query_var( 'a0_action', 'coo-fallback' );
+
+		$output = self::$routes->custom_requests( self::$wp );
+
+		$this->assertContains( '<script src="' . WPA0_AUTH0_JS_CDN_URL . '"></script>', $output );
+		$this->assertContains( 'var auth0 = new auth0.WebAuth({', $output );
+		$this->assertContains( 'clientID:"' . self::$opts->get( 'client_id' ) . '"', $output );
+		$this->assertContains( 'domain:"' . self::$opts->get( 'domain' ) . '"', $output );
+		$this->assertContains( 'redirectUri:"http://example.org/index.php?auth0=1"', $output );
+		$this->assertContains( 'auth0.crossOriginAuthenticationCallback()', $output );
+
+		self::$wp->set_query_var( 'a0_action', null );
+		self::$wp->set_query_var( 'auth0fallback', 1 );
+		$output_2 = self::$routes->custom_requests( self::$wp );
+		$this->assertEquals( $output, $output_2 );
 	}
 }
