ac032e1dd6477988a05615c7bd7ce967b780677a
Josh Cunningham <josh@joshcanhelp.com>
2019-04-16 13:49:40 -0700

    Update database migrations to use remove method and update at the end

--- tests/testWPAuth0DbMigrations.php
+++ tests/testWPAuth0DbMigrations.php
@@ -14,41 +14,6 @@
 class TestWPAuth0DbMigrations extends WP_Auth0_Test_Case {
 
 	/**
-	 * Test a DB upgrade from v18 to v19.
-	 */
-	public function testV19Update() {
-		$test_version        = 19;
-		$initial_connections = [
-			'social_twitter_key'     => '__twitter_key_test__',
-			'social_twitter_secret'  => '__twitter_secret_test__',
-			'social_facebook_key'    => '__facebook_key_test__',
-			'social_facebook_secret' => '__facebook_secret_test__',
-		];
-
-		$connection_keys = array_keys( $initial_connections );
-
-			// Save a 'connections' settings array.
-		self::$opts->set( 'connections', $initial_connections );
-		$saved_connections = self::$opts->get( 'connections' );
-		$this->assertEquals( $initial_connections, $saved_connections );
-
-		// Run the migration for v19.
-		update_option( 'auth0_db_version', $test_version - 1 );
-		$db_manager = new WP_Auth0_DBManager( self::$opts );
-		$db_manager->init();
-
-		foreach ( $connection_keys as $key ) {
-			$this->assertEmpty( self::$opts->get( $key ) );
-		}
-
-		$db_manager->install_db( $test_version, null );
-
-		foreach ( $connection_keys as $key ) {
-			$this->assertEquals( $initial_connections[ $key ], self::$opts->get( $key ) );
-		}
-	}
-
-	/**
 	 * Test a DB upgrade from v19 to v20.
 	 */
 	public function testV20Update() {
