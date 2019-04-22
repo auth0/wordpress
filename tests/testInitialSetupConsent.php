0e04480a941df3c8ec779e5bd40d5adcdcabfa47
Josh Cunningham <josh@joshcanhelp.com>
2019-04-22 15:05:21 -0700

    Failing test for existing migration token

--- tests/testInitialSetupConsent.php
+++ tests/testInitialSetupConsent.php
@@ -204,6 +204,60 @@ class TestInitialSetupConsent extends WP_Auth0_Test_Case {
 		$this->assertCount( 1, self::$error_log->get() );
 	}
 
+	/**
+	 * Test that an connection is created and the Client Grant fails.
+	 */
+	public function testThatNewConnectionIsCreatedWithExistingMigrationToken() {
+		$this->startHttpMocking();
+		$this->startRedirectHalting();
+
+		$setup_consent = new WP_Auth0_InitialSetup_Consent( self::$opts );
+		$test_token    = implode( '.', [ uniqid(), uniqid(), uniqid() ] );
+
+		self::$opts->set( 'client_signing_algorithm', 'HS256' );
+		self::$opts->set( 'migration_token', 'TEST_MIGRATION_TOKEN' );
+
+		// Mock consecutive HTTP calls.
+		$this->http_request_type = [
+			// Successful client creation.
+			'success_create_client',
+			// Get en existing connection enabled for this client.
+			'success_get_connections',
+			// Connection updated successfully.
+			'success_update_connection',
+			// Connection created successfully.
+			'success_create_connection',
+			// Client grant created successfully.
+			'success_create_client_grant',
+		];
+
+		$caught_redirect = [];
+		try {
+			$setup_consent->callback_with_token( 'test-wp.auth0.com', $test_token, 'social' );
+		} catch ( Exception $e ) {
+			$caught_redirect = unserialize( $e->getMessage() );
+		}
+
+		$this->assertNotEmpty( $caught_redirect );
+		$this->assertEquals( 302, $caught_redirect['status'] );
+
+		$redirect_url = parse_url( $caught_redirect['location'] );
+
+		$this->assertEquals( '/wp-admin/admin.php', $redirect_url['path'] );
+		$this->assertContains( 'page=wpa0-setup', $redirect_url['query'] );
+		$this->assertContains( 'step=2', $redirect_url['query'] );
+		$this->assertContains( 'profile=social', $redirect_url['query'] );
+
+		$this->assertEquals( 'TEST_CLIENT_ID', self::$opts->get( 'client_id' ) );
+		$this->assertEquals( 'TEST_CLIENT_SECRET', self::$opts->get( 'client_secret' ) );
+		$this->assertEquals( 1, self::$opts->get( 'db_connection_enabled' ) );
+		$this->assertEquals( 'TEST_CREATED_CONN_ID', self::$opts->get( 'db_connection_id' ) );
+		$this->assertEquals( 'TEST_MIGRATION_TOKEN', self::$opts->get( 'migration_token' ) );
+		$this->assertEquals( 'DB-' . get_auth0_curatedBlogName(), self::$opts->get( 'db_connection_name' ) );
+
+		$this->assertEmpty( self::$error_log->get() );
+	}
+
 	/*
 	 * Test helper functions.
 	 */
@@ -211,7 +265,9 @@ class TestInitialSetupConsent extends WP_Auth0_Test_Case {
 	/**
 	 * Specific mock API responses for this suite.
 	 *
-	 * @return array|null|WP_Error
+	 * @return array|WP_Error
+	 *
+	 * @throws Exception If asked to do so.
 	 */
 	public function httpMock() {
 		$response_type = $this->getResponseType();
@@ -227,6 +283,14 @@ class TestInitialSetupConsent extends WP_Auth0_Test_Case {
 					'body'     => '[{"id":"TEST_CONN_ID","name":"DB-' . get_auth0_curatedBlogName() .
 						'","enabled_clients":["TEST_CLIENT_ID"],"options":{"passwordPolicy":"good"}}]',
 					'response' => [ 'code' => 200 ],
+				];
+
+			case 'success_create_client_grant':
+				$audience = 'https://' . self::$opts->get( 'domain' ) . '/api/v2/';
+				return [
+					'body'     => '{"id": "TEST_CLIENT_GRANT_ID","client_id": "TEST_CLIENT_ID",
+						"audience": "' . $audience . '","scope": ["read:users","update:users"]}',
+					'response' => [ 'code' => 201 ],
 				];
 		}
 
