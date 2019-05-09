fd00902427716e371d04cf984d2c9b7ee7684f91
Josh Cunningham <josh@joshcanhelp.com>
2019-05-09 11:09:30 -0700

    Failing tests for can_show_wp_login_form

---
+++ tests/testOptionCanShowWpLogin.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Contains Class TestOptionCanShowWpLogin.
+ *
+ * @package WP-Auth0
+ *
+ * @since 3.11.0
+ */
+
+/**
+ * Class TestOptionCanShowWpLogin.
+ */
+class TestOptionCanShowWpLogin extends WP_Auth0_Test_Case {
+
+	public function testThatWpLoginCanBeShownIfPluginNotReady() {
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCanBeShownIfOnResetpassPage() {
+		self::auth0Ready();
+		$GLOBALS['pagenow'] = 'wp-login.php';
+		$_REQUEST['action'] = 'resetpass';
+		self::$opts->set( 'wordpress_login_enabled', 'link' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCanBeShownIfOnRpPage() {
+		self::auth0Ready();
+		$GLOBALS['pagenow'] = 'wp-login.php';
+		$_REQUEST['action'] = 'rp';
+		self::$opts->set( 'wordpress_login_enabled', 'link' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCannotBeShownIfNotWle() {
+		self::auth0Ready();
+		self::$opts->set( 'wordpress_login_enabled', 'link' );
+		$this->assertFalse( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCannotBeShownIfWleOff() {
+		self::auth0Ready();
+		$_REQUEST['wle'] = '__test_wle_code__';
+		self::$opts->set( 'wordpress_login_enabled', 'no' );
+		$this->assertFalse( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCanBeShownIfWlePresent() {
+		self::auth0Ready();
+		$_REQUEST['wle'] = '';
+
+		self::$opts->set( 'wordpress_login_enabled', 'link' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+
+		self::$opts->set( 'wordpress_login_enabled', 'isset' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCanBeShownIfWleCodeMatches() {
+		self::auth0Ready();
+		$_REQUEST['wle'] = '__test_wle_code__';
+		self::$opts->set( 'wordpress_login_enabled', 'code' );
+		self::$opts->set( 'wle_code', '__test_wle_code__' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatWpLoginCannotBeShownIfWleCodeNotMatches() {
+		self::auth0Ready();
+		$_REQUEST['wle'] = '__invalid_wle_code__';
+		self::$opts->set( 'wordpress_login_enabled', 'code' );
+		self::$opts->set( 'wle_code', '__test_wle_code__' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+	}
+
+	public function testThatCanShowWpLoginFormReturnsCorrectly() {
+
+		$this->assertEquals( 'link', self::$opts->get( 'wordpress_login_enabled' ) );
+		$_GET['wle'] = '';
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+
+		self::$opts->set( 'wordpress_login_enabled', 'isset' );
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+
+		self::$opts->set( 'wordpress_login_enabled', 'code' );
+		$wle_code = uniqid();
+		self::$opts->set( 'wle_code', $wle_code );
+		$this->assertFalse( self::$opts->can_show_wp_login_form() );
+
+		$_GET['wle'] = $wle_code;
+		$this->assertTrue( self::$opts->can_show_wp_login_form() );
+
+		self::$opts->set( 'wordpress_login_enabled', 'no' );
+		$this->assertFalse( self::$opts->can_show_wp_login_form() );
+	}
+}
--- tests/testWPAuth0Options.php
+++ tests/testWPAuth0Options.php
@@ -198,29 +198,4 @@ class TestWPAuth0Options extends WP_Auth0_Test_Case {
 		$db_options = get_option( $opts->get_options_name() );
 		$this->assertEquals( '__test_domain__', $db_options['domain'] );
 	}
-
-	/**
-	 * Test that the can_show_wp_login_form returns the right bool depending on settings and globals.
-	 */
-	public function testThatCanShowWpLoginFormReturnsCorrectly() {
-		$this->assertFalse( self::$opts->can_show_wp_login_form() );
-
-		$this->assertEquals( 'link', self::$opts->get( 'wordpress_login_enabled' ) );
-		$_GET['wle'] = '';
-		$this->assertTrue( self::$opts->can_show_wp_login_form() );
-
-		self::$opts->set( 'wordpress_login_enabled', 'isset' );
-		$this->assertTrue( self::$opts->can_show_wp_login_form() );
-
-		self::$opts->set( 'wordpress_login_enabled', 'code' );
-		$wle_code = uniqid();
-		self::$opts->set( 'wle_code', $wle_code );
-		$this->assertFalse( self::$opts->can_show_wp_login_form() );
-
-		$_GET['wle'] = $wle_code;
-		$this->assertTrue( self::$opts->can_show_wp_login_form() );
-
-		self::$opts->set( 'wordpress_login_enabled', 'no' );
-		$this->assertFalse( self::$opts->can_show_wp_login_form() );
-	}
 }
