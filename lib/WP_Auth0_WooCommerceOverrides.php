1bf5da6cc6a49f140c6635daeff51ee4df436a72
Josh Cunningham <josh@joshcanhelp.com>
2019-07-31 12:03:46 -0700

    Move hooks out of init method; delete init function; add plugin ready check

--- lib/WP_Auth0_WooCommerceOverrides.php
+++ lib/WP_Auth0_WooCommerceOverrides.php
@@ -1,48 +1,73 @@
 <?php
+/**
+ * Contains Class WP_Auth0_WooCommerceOverrides class.
+ *
+ * @package WP-Auth0
+ *
+ * @since 2.0.0
+ */
 
+/**
+ * Class WP_Auth0_WooCommerceOverrides.
+ */
 class WP_Auth0_WooCommerceOverrides {
+
+	/**
+	 * Injected WP_Auth0 instance.
+	 *
+	 * @var WP_Auth0
+	 */
 	protected $plugin;
+
+	/**
+	 * Injected WP_Auth0_Options instance.
+	 *
+	 * @var WP_Auth0_Options
+	 */
 	protected $options;
 
 	/**
 	 * WP_Auth0_WooCommerceOverrides constructor.
 	 *
-	 * @param WP_Auth0              $plugin
-	 * @param null|WP_Auth0_Options $options
+	 * @param WP_Auth0         $plugin - WP_Auth0 instance.
+	 * @param WP_Auth0_Options $options - WP_Auth0_Options instance.
 	 */
-	public function __construct( WP_Auth0 $plugin, $options = null ) {
-		$this->plugin = $plugin;
-		if ( $options == null ) {
-			$this->options = \WP_Auth0_Options::Instance();
-		} else {
-			$this->options = $options;
-		}
+	public function __construct( WP_Auth0 $plugin, WP_Auth0_Options $options ) {
+		$this->plugin  = $plugin;
+		$this->options = $options;
 	}
 
 	/**
-	 * @deprecated - 3.10.0, will move add_action calls out of this class in the next major.
+	 * Render the login form or link to ULP.
 	 *
-	 * @codeCoverageIgnore - Deprecated.
+	 * @param string $redirect_page - Page slug to redirect to after logging in.
 	 */
-	public function init() {
-		add_filter( 'woocommerce_checkout_login_message', [ $this, 'override_woocommerce_checkout_login_form' ] );
-		add_filter( 'woocommerce_before_customer_login_form', [ $this, 'override_woocommerce_login_form' ] );
-	}
-
-	private function render_login_form( $redirectPage ) {
+	private function render_login_form( $redirect_page ) {
 		$this->plugin->render_auth0_login_css();
 		if ( $this->options->get( 'auto_login', false ) ) {
-			// Redirecting to WordPress login area
-			$redirectUrl = get_permalink( wc_get_page_id( $redirectPage ) );
-			$loginUrl    = wp_login_url( $redirectUrl );
+			// Redirecting to WordPress login page.
+			$redirect_url = get_permalink( wc_get_page_id( $redirect_page ) );
+			$login_url    = wp_login_url( $redirect_url );
 
-			printf( "<a class='button' href='%s'>%s</a>", $loginUrl, __( 'Login', 'wp-auth0' ) );
+			printf( "<a class='button' href='%s'>%s</a>", $login_url, __( 'Login', 'wp-auth0' ) );
 		} else {
 			echo $this->plugin->render_form( '' );
 		}
 	}
 
+	/**
+	 * Handle Auth0 login on the checkout form if the plugin is ready.
+	 *
+	 * @param string $html - Original HTML passed to filter.
+	 *
+	 * @return mixed
+	 */
 	public function override_woocommerce_checkout_login_form( $html ) {
+
+		if ( ! WP_Auth0::ready() ) {
+			return $html;
+		}
+
 		$this->render_login_form( 'checkout' );
 
 		if ( wp_auth0_can_show_wp_login_form() ) {
@@ -50,7 +75,19 @@ class WP_Auth0_WooCommerceOverrides {
 		}
 	}
 
+	/**
+	 * Handle Auth0 login on the account form if the plugin is ready.
+	 *
+	 * @param $html string $html - Original HTML passed to filter.
+	 *
+	 * @return mixed
+	 */
 	public function override_woocommerce_login_form( $html ) {
+
+		if ( ! WP_Auth0::ready() ) {
+			return $html;
+		}
+
 		$this->render_login_form( 'myaccount' );
 	}
 }
