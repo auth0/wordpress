713976fac6b4c00dd729040ab51a92ad2028cd98
Josh Cunningham <josh@joshcanhelp.com>
2019-08-13 10:09:02 -0700

    Update auth params method to add filters

--- tests/testLoginManager.php
+++ tests/testLoginManager.php
@@ -47,49 +47,6 @@ class TestLoginManager extends WP_Auth0_Test_Case {
 	}
 
 	/**
-	 * Test that authorize URL params are built and filtered properly.
-	 */
-	public function testAuthorizeParams() {
-		$test_client_id  = uniqid();
-		$test_connection = uniqid();
-		$auth_params     = WP_Auth0_LoginManager::get_authorize_params();
-
-		$this->assertEquals( 'openid email profile', $auth_params['scope'] );
-		$this->assertEquals( 'code', $auth_params['response_type'] );
-		$this->assertEquals( site_url( 'index.php?auth0=1' ), $auth_params['redirect_uri'] );
-		$this->assertArrayNotHasKey( 'auth0Client', $auth_params );
-		$this->assertNotEmpty( $auth_params['state'] );
-
-		$auth_params = WP_Auth0_LoginManager::get_authorize_params( $test_connection );
-		$this->assertEquals( $test_connection, $auth_params['connection'] );
-
-		self::$opts->set( 'client_id', $test_client_id );
-
-		$auth_params = WP_Auth0_LoginManager::get_authorize_params();
-		$this->assertEquals( $test_client_id, $auth_params['client_id'] );
-
-		self::$opts->set( 'auth0_implicit_workflow', 1 );
-		$auth_params = WP_Auth0_LoginManager::get_authorize_params();
-		$this->assertEquals( add_query_arg( 'auth0', 'implicit', site_url( 'index.php' ) ), $auth_params['redirect_uri'] );
-		$this->assertEquals( 'id_token', $auth_params['response_type'] );
-		$this->assertNotEmpty( $auth_params['nonce'] );
-		$this->assertEquals( 'form_post', $auth_params['response_mode'] );
-
-		add_filter(
-			'auth0_authorize_url_params',
-			function( $params, $connection, $redirect_to ) {
-				$params[ $connection ] = $redirect_to;
-				return $params;
-			},
-			10,
-			3
-		);
-
-		$auth_params = WP_Auth0_LoginManager::get_authorize_params( 'auth0', 'https://auth0.com' );
-		$this->assertEquals( 'https://auth0.com', $auth_params['auth0'] );
-	}
-
-	/**
 	 * Test that the authorize URL is built properly.
 	 */
 	public function testBuildAuthorizeUrl() {
