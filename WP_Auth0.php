228d0a553ec821694959c4a7a59018f0bab17a5a
Josh Cunningham <josh@joshcanhelp.com>
2019-12-17 20:13:02 -0800

    Refactor initial setup initialization

--- WP_Auth0.php
+++ WP_Auth0.php
@@ -541,17 +541,17 @@ function wp_auth0_settings_admin_action_error() {
 add_action( 'admin_notices', 'wp_auth0_settings_admin_action_error' );
 
 function wp_auth0_initial_setup_init() {
-	if ( ( ! isset( $_REQUEST['page'] ) ) || ( 'wpa0-setup' !== $_REQUEST['page'] ) || ( ! isset( $_REQUEST['callback'] ) ) ) {
+	if ( 'wpa0-setup' !== ( $_REQUEST['page'] ?? null ) || ! isset( $_REQUEST['callback'] ) ) {
 		return false;
 	}
 
-	if ( isset( $_REQUEST['error'] ) && 'rejected' == $_REQUEST['error'] ) {
-		wp_redirect( admin_url( 'admin.php?page=wpa0-setup&error=rejected' ) );
+	if ( 'rejected' === ( $_REQUEST['error'] ?? null ) ) {
+		wp_safe_redirect( admin_url( 'admin.php?page=wpa0-setup&error=rejected' ) );
 		exit;
 	}
 
-	if ( isset( $_REQUEST['error'] ) && 'access_denied' == $_REQUEST['error'] ) {
-		wp_redirect( admin_url( 'admin.php?page=wpa0-setup&error=access_denied' ) );
+	if ( 'access_denied' === ( $_REQUEST['error'] ?? null ) ) {
+		wp_safe_redirect( admin_url( 'admin.php?page=wpa0-setup&error=access_denied' ) );
 		exit;
 	}
 
