4991ea9d710580e126094a4b2fc2ad52ae33125c
Josh Cunningham <josh@joshcanhelp.com>
2019-12-19 20:22:30 -0800

    Move render form out of WP_Auth0

--- tests/testRenderForm.php
+++ tests/testRenderForm.php
@@ -14,27 +14,23 @@
 class TestRenderForm extends WP_Auth0_Test_Case {
 
 	public function testThatRenderFormPassesThroughIfPluginNotReady() {
-		$wp_auth0 = new WP_Auth0( self::$opts );
-		$this->assertEquals( '__initial_html__', $wp_auth0->render_form( '__initial_html__' ) );
+		$this->assertEquals( '__initial_html__', wp_auth0_render_lock_form( '__initial_html__' ) );
 	}
 
 	public function testThatHtmlIsIgnoredIfAuth0ParamSet() {
-		$wp_auth0 = new WP_Auth0( self::$opts );
 		$this->auth0Ready();
 		$_GET['auth0'] = 1;
-		$this->assertNotEquals( '__initial_html__', $wp_auth0->render_form( '__initial_html__' ) );
+		$this->assertNotEquals( '__initial_html__', wp_auth0_render_lock_form( '__initial_html__' ) );
 	}
 
 	public function testThatHtmlIsIgnoredIfLostpasswordParamSet() {
-		$wp_auth0 = new WP_Auth0( self::$opts );
 		$this->auth0Ready();
 		$_GET['action'] = 'lostpassword';
-		$this->assertNotEquals( '__initial_html__', $wp_auth0->render_form( '__initial_html__' ) );
+		$this->assertNotEquals( '__initial_html__', wp_auth0_render_lock_form( '__initial_html__' ) );
 	}
 
 	public function testThatHtmlIsIgnoredWhenAuth0IsReady() {
-		$wp_auth0 = new WP_Auth0( self::$opts );
 		$this->auth0Ready();
-		$this->assertNotEquals( '__initial_html__', $wp_auth0->render_form( '__initial_html__' ) );
+		$this->assertNotEquals( '__initial_html__', wp_auth0_render_lock_form( '__initial_html__' ) );
 	}
 }
