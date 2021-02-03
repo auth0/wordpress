cf59f6816075d9d4a2bced60064dedd85a909973
Andrea Posi <a.posi@procurementleaders.com>
2021-02-03 12:04:33 +0000

    Pass user info to auth0_before_login action

--- examples/auth0_before_login.php
+++ examples/auth0_before_login.php
@@ -7,9 +7,11 @@
  * @see WP_Auth0_LoginManager::do_login()
  *
  * @param WP_User $user - WordPress user object.
+ * @param stdClass $userinfo - user information object from Auth0
  */
-function example_auth0_before_login( $user ) {
+function example_auth0_before_login( $user, $userinfo ) {
 	echo '<strong>WP user</strong>:<br><pre>' . print_r( $user, true ) . '</pre><hr>';
+	echo '<strong>Auth0 user info</strong>:<br><pre>' . print_r( $userinfo, true ) . '</pre><hr>';
 	wp_die( 'Login process started!' );
 }
- add_action( 'auth0_before_login', 'example_auth0_before_login', 10, 1 );
+ add_action( 'auth0_before_login', 'example_auth0_before_login', 10, 2 );
