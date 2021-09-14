4863997cc283daca2b71e62802a0d4367c747de5
Evan Sims <evan.sims@auth0.com>
2021-09-14 11:07:34 -0400

    [SDK-2788] Update client configuration url for embedded logins (#832)
    * Use custom domain for client configuration
    
    * Use custom domain for client configuration
    
    * Use tenant domain rather than CDN for client settings
    
    * Update lock and auth0-js to latest versions
    
    * Updates to login CSS for new Lock version.
    
    * Updates to CSS
    
    * Updates to CSS
    
    * Fix test warning
    
    * Update tests
    
    * Update tests
    
    * Update tests
    
    * Teseting
    
    * Remove unnecessary configurationBaseUrl to tenant domain
    
    * Test fixes

--- tests/testLockOptions.php
+++ tests/testLockOptions.php
@@ -25,16 +25,10 @@ class TestLockOptions extends WP_Auth0_Test_Case {
 		self::$opts->set( 'domain', 'test.auth0.com' );
 		$lock_options     = new WP_Auth0_Lock( [], self::$opts );
 		$lock_options_arr = $lock_options->get_lock_options();
-		$this->assertArrayNotHasKey( 'configurationBaseUrl', $lock_options_arr );
+		$this->assertNull( $lock_options->get_lock_options()['configurationBaseUrl'] ?? null );
 
 		self::$opts->set( 'custom_domain', 'login.example.com' );
-		$this->assertEquals( 'https://cdn.auth0.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
-
-		self::$opts->set( 'domain', 'test.eu.auth0.com' );
-		$this->assertEquals( 'https://cdn.eu.auth0.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
-
-		self::$opts->set( 'domain', 'test.au.auth0.com' );
-		$this->assertEquals( 'https://cdn.au.auth0.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
+		$this->assertEquals( 'https://login.example.com', $lock_options->get_lock_options()['configurationBaseUrl'] );
 	}
 
 	/**
