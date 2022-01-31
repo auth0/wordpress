065b1f34b770e42033de0a95fd3d9aa78e21d9f0
Alyx Baldwin <jrbaldwin@gmail.com>
2022-02-01 05:35:06 +0800

    Update wpa0_should_create_user.php (#843)
    Co-authored-by: Evan Sims <hello@evansims.com>

--- examples/wpa0_should_create_user.php
+++ examples/wpa0_should_create_user.php
@@ -11,4 +11,4 @@ function example_wpa0_should_create_user( $should_create, $userinfo ) {
 	$should_create = false !== strpos( '@example.com', $userinfo->email );
 	return $should_create;
 }
-add_filter( 'wpa0_should_create_user', 'example_wpa0_should_create_user' );
+add_filter( 'wpa0_should_create_user', 'example_wpa0_should_create_user' . 10, 2 );
