bce61190bfa1d74beb4124d75d587ae3b5dd2155
Evan Sims <evan.sims@okta.com>
2022-12-12 17:26:16 -0600

    Merge functions.php into wpAuth0.php

--- wpAuth0.php
+++ wpAuth0.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:       Auth0
- * Plugin URL:        https://github.com/auth0/wp-auth0
+ * Plugin URL:        https://github.com/auth0/wordpress
  * Description:       Supercharge your WordPress website with Auth0. Improve account security, add support for multifactor, enable social, passwordless and enterprise connections, and much more.
  * Version:           5.0.0
  * Requires at least: 6.0
@@ -12,12 +12,16 @@
  * Author:            Auth0
  * Author URI:        https://auth0.com
  * License:           MIT
- * License URI:       https://github.com/auth0/wp-auth0/blob/master/LICENSE
+ * License URI:       https://github.com/auth0/wordpress/blob/master/LICENSE
  * Text Domain:       wp-auth0
  * Domain Path:       /languages
  */
 
 declare(strict_types=1);
+
+use Auth0\WordPress\Plugin;
+use Auth0\SDK\Auth0 as Sdk;
+use Auth0\SDK\Configuration\SdkConfiguration as Configuration;
 
 define('WP_AUTH0_VERSION', '5.0.0');
 
@@ -29,8 +33,37 @@ if (! defined('ABSPATH')) {
 // Load dependencies
 require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 
-// Load plugin helper functions
-require_once plugin_dir_path(__FILE__) . 'functions.php';
+register_activation_hook(
+	__FILE__,
+    function() {
+        $cookies = get_option('auth0_cookies', []);
+
+        if (! is_array($cookies) || [] === $cookies || ! isset($cookies['secret'])) {
+            add_option('auth0_cookies', [
+                'secret' => bin2hex(random_bytes(64))
+            ]);
+        }
+    }
+);
 
 // Run plugin functions
 wpAuth0()->run();
+
+/**
+ * Return a configured singleton of the Auth0 WP plugin.
+ *
+ * @param null|Plugin $plugin Optional. An existing instance of Auth0\WordPress\Plugin to use.
+ * @param null|Auth0\SDK\Auth0 $sdk Optional. An existing instance of Auth0\SDK\Auth0 to use.
+ * @param null|Auth0\SDK\Configuration\SdkConfiguration $configuration Optional. An existing instance of Auth0\SDK\Configuration\SdkConfiguration to use.
+ */
+function wpAuth0(
+    ?Plugin $plugin = null,
+    ?Sdk $sdk = null,
+    ?Configuration $configuration = null,
+): Plugin {
+    static $instance = null;
+
+    $instance ??= $instance ?? $plugin ?? new Plugin($sdk, $configuration);
+
+    return $instance;
+}
